c4d85cd6ab4ee7957cc77a06d05dd721904776dd
Alexander Abushady <aabushady214@gmail.com>
2019-07-16 05:38:10 -0400

    SoundViewController
    Set up Sound View Controller. Added buttons and text field. Linked to main view controller with a show segue. Added navigation view controller. Added add button on main view controller.

---
+++ Soundboard/Soundboard/SoundViewController.swift
@@ -0,0 +1,27 @@
+//
+//  SoundViewController.swift
+//  Soundboard
+//
+//  Created by Alexander Abushady on 7/16/19.
+//  Copyright © 2019 Alexander Abushady. All rights reserved.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+  
+    @IBAction func recordTapped(_ sender: Any) {
+    }
+    @IBAction func playTapped(_ sender: Any) {
+    }
+    @IBAction func addTapped(_ sender: Any) {
+    }
+}
