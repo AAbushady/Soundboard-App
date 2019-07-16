670bd3227de97fb766e3cd67812d3e4cad364b1f
Alexander Abushady <aabushady214@gmail.com>
2019-07-16 09:47:47 -0400

    Finishing up.
    Added functionality for deleting from Core Data and the table view. Cleaned up code and added comments throughout as well.

--- Soundboard/Soundboard/SoundViewController.swift
+++ Soundboard/Soundboard/SoundViewController.swift
@@ -10,11 +10,13 @@ import UIKit
 import AVFoundation
 
 class SoundViewController: UIViewController {
+    // Setting outlets for each input field.
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var addButton: UIButton!
     
+    // Variables for recording, playing, and storing audio.
     var audioRecorder : AVAudioRecorder?
     var audioPlayer : AVAudioPlayer?
     var audioURL : URL?
@@ -22,11 +24,13 @@ class SoundViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Sets up the audio recorder and makes sure that the appropriate buttons are disabled.
         setupRecorder()
         playButton.isEnabled = false
         addButton.isEnabled = false
     }
     
+    // Adds functionality to record audio.
     func setupRecorder() {
         do {
             // Create an audio session.
@@ -54,6 +58,7 @@ class SoundViewController: UIViewController {
         }
     }
     
+    // Record audio from microphone when record is tapped.
     @IBAction func recordTapped(_ sender: Any) {
         if audioRecorder!.isRecording {
             // Stop the recording.
@@ -62,6 +67,7 @@ class SoundViewController: UIViewController {
             // Change button title to record.
             recordButton.setTitle("Record", for: .normal)
             
+            // Enable appropriate buttons.
             playButton.isEnabled = true
             addButton.isEnabled = true
         } else {
@@ -70,25 +76,36 @@ class SoundViewController: UIViewController {
             // Change button title to stop.
             recordButton.setTitle("Stop", for: .normal)
             
+            // Disable appropriate buttons.
             playButton.isEnabled = false
             addButton.isEnabled = false
         }
     }
+    
+    // Play audio when play is tapped.
     @IBAction func playTapped(_ sender: Any) {
         do {
+            // Pulls the audio from the audioURL to play it.
             try audioPlayer = AVAudioPlayer(contentsOf: audioURL!)
             audioPlayer!.play()
         } catch {}
     }
+    
+    // When add is tapped the audio is then sent to Core Data.
     @IBAction func addTapped(_ sender: Any) {
+        // Connect to Core Data.
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        // Allows for creation of a Sound Entity.
         let sound = Sound(context: context)
         
+        // Create a Sound Entitiy.
         sound.name = nameTextField.text
         sound.audio = NSData(contentsOf: audioURL!) as Data?
         
+        // Update Core Data with the new Sound entity.
         (UIApplication.shared.delegate as! AppDelegate).saveContext()
-        
+
+        // After adding to Core Data return to the main view controller.
         navigationController!.popViewController(animated: true)
     }
 }
--- Soundboard/Soundboard/ViewController.swift
+++ Soundboard/Soundboard/ViewController.swift
@@ -10,19 +10,22 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-
+    // Outlet to the table view.
     @IBOutlet weak var tableView: UITableView!
     
+    // Variables for working with Core Data, and playing audio.
     var sounds : [Sound] = []
     var audioPlayer : AVAudioPlayer?
     
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        // Set delegate and data source within the View Controller.
         tableView.dataSource = self
         tableView.delegate = self
     }
     
+    // Reload the table view when Core Data is changed.
     override func viewWillAppear(_ animated: Bool) {
         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
         do {
@@ -31,28 +34,40 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         } catch {}
     }
     
+    // Set the number of rows in the table view to the amount of entities in Core Data.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return sounds.count
     }
     
+    // Dictates what is placed within each cell of the table view.
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         let sound = sounds[indexPath.row]
-        
         cell.textLabel?.text = sound.name
-        
         return cell
     }
 
+    // When selecting a row play audio.
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let sound = sounds[indexPath.row]
         do {
             audioPlayer = try AVAudioPlayer(data: sound.audio! as Data)
             audioPlayer?.play()
         } catch {}
-        
         tableView.deselectRow(at: indexPath, animated: true)
     }
     
+    // Functionality for swiping to delete a Core Data Entity. Also updates the table view with the updated array from Core Data.
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let sound = sounds[indexPath.row]
+            let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+            context.delete(sound)
+            (UIApplication.shared.delegate as! AppDelegate).saveContext()
+            do {
+                sounds = try context.fetch(Sound.fetchRequest())
+                tableView.reloadData()
+            } catch {}
+        }
+    }
 }
-
